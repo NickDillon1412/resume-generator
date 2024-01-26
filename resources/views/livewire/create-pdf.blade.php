29314eff7933f41932168bd24ecac8abd5cd9a85
Nick Dillon <nickhds@gmail.com>
2024-01-26 13:26:56 -0600

    Create Livewire component

---
+++ resources/views/livewire/create-pdf.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <button
+        class="flex items-center px-5 py-1.5 rounded bg-indigo-500 hover:bg-indigo-600 shadow transition ease-in-out duration-200 text-white">
+        <svg class="w-4 h-4 opacity-50 fill-current shrink-0" viewBox="0 0 16 16">
+            <path
+                d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z">
+            </path>
+        </svg>
+        <span class="ml-2">Create PDF</span>
+    </button>
+</div>
