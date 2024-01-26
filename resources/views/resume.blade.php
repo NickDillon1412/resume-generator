6859f7d366ef2777a4c6235e65ad5c463a742901
Nick Dillon <nickhds@gmail.com>
2024-01-26 13:23:21 -0600

    Added create pdf button

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -15,6 +15,18 @@
 </head>
 
 <body class="antialiased min-h-screen">
+    <div class="flex justify-end top-7 right-7 absolute">
+        <button
+            class="flex items-center px-5 py-1.5 rounded bg-indigo-500 hover:bg-indigo-600 shadow transition ease-in-out duration-200 text-white">
+            <svg class="w-4 h-4 opacity-50 fill-current shrink-0" viewBox="0 0 16 16">
+                <path
+                    d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z">
+                </path>
+            </svg>
+            <span class="ml-2">Create PDF</span>
+        </button>
+    </div>
+
     <div class="mx-auto p-16 bg-white max-w-[925px]">
         <h1 class="font-bold text-center text-4xl">Nick Dillon</h1>
 
